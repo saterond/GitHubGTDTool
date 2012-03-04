7b2e0a04767d1aea704734dc4d7cd0889019e6c0
Ondrej Satera <ondrej.satera@gmail.com>
2012-03-04 19:42:26 +0100

    Unit testy - model

--- src/Resources/tests/modelTest.js
+++ src/Resources/tests/modelTest.js
@@ -1,29 +1,40 @@
 /**
  * @author saterond
  */
+var configFile = Titanium.Filesystem.getFile(Titanium.Filesystem.getResourcesDirectory(), 'config.json');
+
+if (!configFile.exists()) 
+	console.log("Config file's missing!");
+var config = Titanium.JSON.parse(configFile.read());
+var db = Titanium.Database.open(config.database);
+var model = new GTDModel(db);
+
 var DataTestSuite = {
-    suiteName: "Data Test Suite",
-    db: null,
-    config: null,
- 
+    suiteName: "Data Test Suite",       
+    
     setUp: function() {
- 		var resourcesDir = Titanium.Filesystem.getResourcesDirectory();	
-	    var configFile = Titanium.Filesystem.getFile(resourcesDir, 'config.json');
-	    
-	    if (!configFile.exists()) {
-        	console.log("Config file's missing!");
-    	}
-        var configJSON = configFile.read();
-        this.config = Titanium.JSON.parse(configJSON);
-        this.db = Titanium.Database.open(this.config.database);
     },
  
     tearDown: function() {
- 		this.db.close();
     },
  
-    testInit: function() {
- 		jsUnity.assertions.assertTrue(true);
-    }
+    testGetProjects: function() {
+ 		var all = db.execute("SELECT count(project_id) as pocet FROM project");
+ 		var db_count = all.fieldByName("pocet");
+ 		var projects = model.getProjects();
+ 		var mo_count = projects.length;
+ 		jsUnity.assertions.assertEqual(db_count, mo_count, "Model have found different count of projects");
+    },
     
+    testGetProject: function() {
+    	var one = db.execute("SELECT project_id,name FROM project LIMIT 1");
+    	var project_id = one.fieldByName("project_id");
+    	var project_name = one.fieldByName("name");
+    	var params = new Object();
+    	params["project_id"] = project_id;
+    	var mo_one = model.getProject(params);
+    	
+    	jsUnity.assertions.assertEqual(project_name, mo_one.name, "Project names doesn't match");
+    	db.close();
+    }
 };
